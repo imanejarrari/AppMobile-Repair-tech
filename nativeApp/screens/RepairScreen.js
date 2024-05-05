cd2e3b1d5ba04cae1c4fcce8bedf4bb4204f73fd
imanejarrari <benjarrariimane@gmail.com>
2024-05-05 16:17:09 +0100

    commit

--- nativeApp/screens/RepairScreen.js
+++ nativeApp/screens/RepairScreen.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ScrollView ,Image} from 'react-native';
-import { TouchableOpacity } from 'react-native';
-import { query, collection, getDocs,orderBy,limit } from "firebase/firestore";
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { query, collection, getDocs } from "firebase/firestore";
 import { db } from '../firebase/firebase';
-import 'firebase/firestore';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+
 const RepairScreen = ({ navigation }) => { 
   const [technicians, setTechnicians] = useState([]);
-  const [latestRepair, setLatestRepair] = useState([]);
 
   useEffect(() => {
     const fetchTechnicians = async () => {
@@ -22,50 +20,12 @@ const RepairScreen = ({ navigation }) => {
     };
 
     fetchTechnicians();
-
-    const fetchLatestRepair = async () => {
-      try {
-        const q = query(collection(db, "RepairRequest"));
-        const querySnapshot = await getDocs(q);
-        if (!querySnapshot.empty) {
-          const latestRepairData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-          setLatestRepair(latestRepairData);
-        } else {
-          setLatestRepair(null);
-        }
-      } catch (error) {
-        console.error('Error fetching latest repair request:', error);
-      }
-    };
-
-    fetchLatestRepair();
   }, []);
 
-  // Function to handle navigation when "Go to Repair" button is pressed
-  const goToRepairForm = () => {
-    navigation.navigate('RepairForm');
-  };
-  //status colors
-  const getStatusStyle = (status) => {
-    switch (status) {
-      case 'completed':
-        return { color: 'black' , borderWidth:1,borderColor:"green",backgroundColor:"green",width:100,paddingLeft:15,borderRadius:30,marginTop:15,marginLeft:25 }
-      case 'pending':
-        return { color: 'black' , borderWidth:1,borderColor:"yellow",backgroundColor:"yellow",width:100,paddingLeft:25,borderRadius:30,marginTop:15,marginLeft:25 };
-      case 'in progress':
-        return { color: 'black' , borderWidth:1,borderColor:"orange",backgroundColor:"orange",width:100,paddingLeft:16,borderRadius:30,marginTop:15,marginLeft:25 }
-      default:
-        return {}; 
-    }
-  };
-   // Function to handle send msg
-   const handleSendMessage = (technicianId) => {
-   
+  const handleSendMessage = (technicianId) => {
     navigation.navigate('ChatScreen', { technicianId });
   };
-  
 
-  // Function to handle Schedule Meeting
   const handleScheduleMeeting = (technicianId) => {
     navigation.navigate('MeetingFormScreen', { technicianId });
   };
@@ -76,56 +36,58 @@ const RepairScreen = ({ navigation }) => {
         <Text style={styles.head}>Repair & Recovery </Text>
       </View>
       <View style={styles.contentContainer}>
-         <ScrollView >
+        <ScrollView>
           <View style={styles.RepairAnn}>
-            <Text style={{fontWeight:'bold',fontSize:25 ,color:'#8B322C' ,paddingLeft:30 , borderBottomWidth:1 , width:220 ,marginLeft:5 ,borderColor:'#8B322C'}}>Tech Repair</Text>
-            <View style={{ flexDirection: 'row', alignItems: 'center' ,width:250 ,paddingLeft:5}}>
-                 <Text style={{ fontSize:13, letterSpacing:0.1 }}>Your Partner For Your Acquisitions Of Computer Equipment, Office Automation, Accessories, Consumables And Stationery.  </Text>
-                    <Ionicons name="construct-outline" size={70} color="grey" style={{ marginLeft: 2 }} />
-           </View>
-            
-            
-           <TouchableOpacity style={styles.button} onPress={goToRepairForm}>
-                <Text style={{fontSize:15,color:'black',fontWeight:'bold',marginTop:5}}>Go to Repair {'>>>'}</Text>
-              </TouchableOpacity>
-
+            <Text style={styles.repairTitle}>Tech Repair</Text>
+            <View style={styles.repairDescription}>
+              <Text style={styles.repairText}>Your Partner For Your Acquisitions Of Computer Equipment, Office Automation, Accessories, Consumables And Stationery.</Text>
+              <Image source={require('../repair.png')} style={styles.img}/>
+            </View>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('RepairForm')}>
+              <Text style={styles.buttonText}>Go to Repair {'>>>'}</Text>
+            </TouchableOpacity>
           </View>
+          <View style={styles.technicianContainer}>
+        
+          
+            <ScrollView horizontal={true} contentContainerStyle={styles.scrollContainer}>
+              <View style={styles.tableContainer}>
 
-    
-     
-    
-
-       <View style={styles.technicianContainer}>
-         <Text style={styles.technicianTitle}>Technicians</Text>
-         <ScrollView horizontal={true} contentContainerStyle={styles.scrollContainer}>
-         <View style={styles.cardContainer}>
-           {technicians.map(technician => (
-             <View key={technician.id} style={styles.requestTech}>
-                <Image 
+              <View style={{flexDirection: 'row',alignItems: 'center',}}>
+                  <Image 
+                   source={require('../technician.png')}
+                    style={styles.avatar1}
+                 />
+                  <Text style={styles.technicianTitle}>
+                    Technicians
+                   </Text>
+                
+                </View>
+              
+              
+               
+                {technicians.map(technician => (
+                  <View key={technician.id} style={styles.tableRow}>
+                     <Image 
                    source={require('../picture.png')}
                     style={styles.avatar}
                  />
-               <Text style={{fontWeight:'bold',marginLeft:25,marginTop:5,fontSize:15}}>{technician.Name}</Text>
-               <Text style={{marginLeft:20,marginTop:3 ,fontSize:13}}>{technician.Specialization}</Text>
-               <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-               <TouchableOpacity onPress={() => handleSendMessage(technician.id)}>
-                  <Ionicons name="chatbox-outline" size={24} color="green" style={{ marginLeft: 30 ,marginTop:10}} />
-               </TouchableOpacity>
-               <TouchableOpacity onPress={() => handleScheduleMeeting(technician.id)}>
-                  <Ionicons name="calendar-outline" size={22} color="green" style={{marginLeft: 20,marginTop:10}} />
-              </TouchableOpacity>
-
-               </View>
-             </View>
-           ))}
-         </View>
-       </ScrollView>
-       </View>
-
-
+                    <Text style={styles.column}>{technician.Name}</Text>
+                    <Text style={styles.column2}  >{technician.Specialization}</Text>
+                    <View style={styles.actions}>
+                      <TouchableOpacity onPress={() => handleSendMessage(technician.id)}>
+                        <Ionicons name="chatbox-outline" size={20} color="#8B322C" style={{marginLeft:10}}  />
+                      </TouchableOpacity>
+                      <TouchableOpacity onPress={() => handleScheduleMeeting(technician.id)}>
+                        <Ionicons name="calendar-outline" size={20} color="#8B322C" style={{marginLeft:10}} />
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                ))}
+              </View>
+            </ScrollView>
+          </View>
         </ScrollView>
-
-        
       </View>
     </View>
   );
@@ -150,60 +112,157 @@ const styles = StyleSheet.create({
     letterSpacing: 1.5,
     lineHeight: 24,
     marginTop: 40,
-    fontWeight:'bold',
+    fontWeight: 'bold',
   },
-  
-  scrollContainer: {
-    alignItems: 'flex-end',
+  contentContainer: {
+    flex: 1,
   },
-
-
-
-
-  technicianContainer: {    
+  RepairAnn: {
+    marginTop: 20,
+    marginLeft: 10,
+    width: 340,
+    height: 180,
+    borderWidth: 0.1,
+    backgroundColor: 'white',
+    elevation: 6,
+    shadowColor: 'black',
+    shadowOpacity: 1,
+    shadowRadius: 10,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    padding: 20,
+    paddingLeft:10
+  },
+  repairTitle: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    color: '#8B322C',
+    paddingLeft: 30,
+    borderBottomWidth: 1,
+    width: 220,
+    marginLeft: 5,
+    borderColor: '#8B322C',
+  },
+  repairDescription: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: 250,
+    paddingLeft: 5,
+  },
+  repairText: {
+    fontSize: 13,
+    letterSpacing: 0.1,
+  },
+  img: {
+    width: 70,
+    height: 70,
+    marginRight: 5,
+  },
+  button: {
+    borderWidth: 1,
+    width: 150,
+    height: 33,
+    borderRadius: 15,
+    borderColor: '#5BBCFF',
+    backgroundColor: '#5BBCFF',
+    marginTop: 5,
+    marginLeft: 30,
+    paddingLeft: 20,
+    paddingBottom: 5,
+  },
+  buttonText: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+  technicianContainer: {
     marginTop: 20,
   },
   technicianTitle: {
-    fontSize: 15,
+    fontSize: 25,
     fontWeight: 'bold',
-    marginBottom: 10,
     paddingHorizontal: 20,
-    color:'#8B322C'
+    color: '#8B322C',
+    width:330,
+    height:40,
+    marginLeft:10,
+    paddingLeft:50,
+    letterSpacing:1,
+    borderBottomWidth:6,
+    borderColor:'#8B322C'
   },
-  RepairAnn:{
-    marginLeft:5,
-    marginTop:20,
-    width: 350,
-    height: 200,
-    borderWidth:0.10,
-    backgroundColor: 'white ',
-    elevation: 6,
-    shadowColor:'#5BBCFF',
+  scrollContainer: {
+    alignItems: 'flex-end',
+  },
+  tableContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderRadius: 5,
+    padding: 5,
+    margin: 10,
+    backgroundColor: 'white',
+    elevation: 5,
+    shadowColor: 'black',
     shadowOpacity: 1,
-    shadowRadius: 1,
+    shadowRadius: 5,
     shadowOffset: {
-      width: 0,   
-      height: 2, 
+      width: 0,
+      height: 2,
     },
-    padding:20
-  
   },
-  button:{
-    borderWidth:1,
-    width:120,
-    height:35,
+ 
+  columnHeader: {
+    flex: 1,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  tableRow: {
+    flexDirection: 'row',
+    borderBottomWidth: 0.5,
+    borderBottomColor: 'grey',
+    padding: 5,
+  },
+  column: {
+    flex: 1,
+    padding: 7,
     paddingLeft:5,
-    borderRadius:15,
-    borderColor:'#5BBCFF',
-    backgroundColor:'#5BBCFF',
-    marginTop:15,
-    marginLeft:50
+    fontSize:13,
+    
+  },
+  column2: {
+    flex: 1,
+    padding: 5,
+    paddingLeft:10,
+    color:'black',
+    fontWeight:'bold',
+    fontSize:14,
+    letterSpacing:0.5,
+   
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flex: 1,
   },
   avatar: {
     width: 40,
     height: 40,
     borderRadius: 15,
-    marginLeft:44,
   },
-  
+  avatar1: {
+    width: 35,
+    height: 35,
+    borderRadius: 15,
+    position:'absolute',
+    bottom:10,
+    left:20
+
+  },
+
 });
