e91fcd5b7dfc834d21bdc8d67d4a6402e680ea15
imanejarrari <benjarrariimane@gmail.com>
2024-05-05 13:19:30 +0100

    CC

--- nativeApp/screens/HomeScreen.js
+++ nativeApp/screens/HomeScreen.js
@@ -176,7 +176,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginTop: 60,
+    marginTop: 40,
     marginBottom: 10,
   },
   searchInput: {
