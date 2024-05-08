d8f5db843ae9568c5477e9b39ff4821032707d7f
imanejarrari <benjarrariimane@gmail.com>
2024-05-08 02:14:15 +0100

    picture

--- nativeApp/screens/ProfileScreen.js
+++ nativeApp/screens/ProfileScreen.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, TouchableOpacity, Alert ,StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Alert, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { auth, db } from '../firebase/firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
-import { LinearGradient } from 'expo-linear-gradient'; // Import LinearGradient
+import { LinearGradient } from 'expo-linear-gradient';
+import { MaterialIcons } from '@expo/vector-icons'; // Import Material Icons or any other icon library
 
 const ProfileScreen = () => {
   const [userData, setUserData] = useState(null);
@@ -35,13 +36,13 @@ const ProfileScreen = () => {
   }, []);
 
   const handleEditProfile = () => {
-    navigation.navigate('EditProfileScreen'); 
+    navigation.navigate('EditProfileScreen');
   };
 
   return (
-    <View style={{ flex: 1 , backgroundColor:'whitesmoke'}}>
+    <View style={{ flex: 1 }}>
       {userData ? (
-        <View  >
+        <View>
           <LinearGradient colors={['#8B322C', '#FF9999']} style={Styles.header}>
             {userData.profilePicture ? (
               <Image
@@ -50,25 +51,38 @@ const ProfileScreen = () => {
               />
             ) : (
               <Image
-                source={require('../userProfile.png')}
-                style={{ width: 150, height: 150, marginTop: 150,marginLeft:100 }}
+                source={require('../picture.png')}
+                style={{ width: 140, height: 140, marginTop: 310, marginLeft: 120 }}
               />
             )}
-            <Text  style={{ fontSize:20,letterSpacing:1
-            }}>  {userData.firstName}</Text>
+            <Text style={{ fontSize: 20, letterSpacing: 1, top: -200, left: 160, fontWeight: 'bold' }}>{userData.firstName}</Text>
           </LinearGradient>
 
-          <View  style={Styles.contt}>
-            <Text   style={{fontWeight:'bold' , fontSize:20,letterSpacing:1 , marginLeft:80,marginTop:20 }}>USER PROFILE</Text>
-
-            <Text>Email: {userData.email}</Text>
-          <Text>First Name: {userData.firstName}</Text>
-          <Text>Last Name: {userData.lastName}</Text>
-          <TouchableOpacity onPress={handleEditProfile} style={{ marginTop: 20 }}>
-            <Text>Edit Profile</Text>
-          </TouchableOpacity>
+          <View style={Styles.contt}>
+            <View style={Styles.userInfo}>
+              <MaterialIcons name="person" size={24} color="#FF9999" />
+              <Text style={Styles.userInfoText}>{userData.firstName}</Text>
+            </View>
+            <View style={Styles.userInfo}>
+              <MaterialIcons name="person" size={24} color="#FF9999" />
+              <Text style={Styles.userInfoText}>{userData.lastName}</Text>
+            </View>
+            <View style={Styles.userInfo}>
+              <MaterialIcons name="email" size={24} color="#FF9999" />
+              <Text style={Styles.userInfoText}>{userData.email}</Text>
+            </View>
+            <View style={Styles.userInfo}>
+              <MaterialIcons name="phone" size={24} color="#FF9999" />
+              <Text style={Styles.userInfoText}>00000000000</Text>
+            </View>
+            <View style={Styles.userInfo}>
+              <MaterialIcons name="security" size={24} color="#FF9999" />
+              <Text style={Styles.userInfoText}>121212121212</Text>
+            </View>
+            <TouchableOpacity onPress={handleEditProfile} style={Styles.editProfileButton}>
+              <Text style={{ color:'white',paddingLeft: 65, paddingTop: 8 }}>Edit Profile</Text>
+            </TouchableOpacity>
           </View>
-          
         </View>
       ) : (
         <Text>Loading...</Text>
@@ -82,23 +96,46 @@ export default ProfileScreen;
 const Styles = StyleSheet.create({
   header: {
     position: 'absolute',
-    left: 0,
-    right: 0,
-    height: 250,
-    borderBottomLeftRadius:200,
-    borderBottomRightRadius:200
+    left: -20,
+    right: 10,
+    height: 400,
+    top: -170,
+    width: 400,
+    borderRadius: 200
   },
 
-  contt:{
+  contt: {
     position: 'absolute',
-    top: 350,
-    left: 30,
-    right: 30,
+    top: 290,
+    left: 20,
+    right: 20,
     height: 430,
-    backgroundColor:'white',
-    borderRadius:10,
-    borderWidth:1,
-    borderColor:'white'
+  },
+
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 0.5,
+    height: 60,
+    borderColor: 'grey',
+    letterSpacing: 1,
+    paddingLeft: 40,
+    paddingTop: 30
+  },
+
+  userInfoText: {
+    marginLeft: 50
+  },
+
+  editProfileButton: {
+    backgroundColor: '#FF9999',
+    borderColor: '#FF9999',
+    marginTop: 50,
+    marginLeft: 60,
+    borderWidth: 1,
+    height: 40,
+    borderRadius: 50,
+    width: 200,
     
   }
 });
