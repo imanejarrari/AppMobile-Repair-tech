db74e40ddab3a1254a9775557c3ae4f5e846454c
imanejarrari <benjarrariimane@gmail.com>
2024-05-11 01:49:18 +0100

    commit

--- adminapp/src/components/auth/signUp.js
+++ adminapp/src/components/auth/signUp.js
@@ -2,15 +2,26 @@ import React, { useState } from 'react';
 import { auth } from '../firebase/firebase';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Logo from '../assests/logo.png';
 import "./Auth.css";
+
 const SignUpForm = ({ setisLoggedIn }) => {
+  const [authMode, setAuthMode] = useState('signin'); // Initialize authMode state to 'signin'
   const [formData, setFormData] = useState({
     email: '',
     password: '',
     password2: '',
   });
   const [errors, setErrors] = useState({});
+
+  const changeAuthMode = () => {
+    setAuthMode((prevMode) => (prevMode === 'signin' ? 'signup' : 'signin')); // Toggle authMode between 'signin' and 'signup'
+    setErrors({});
+    setFormData({
+      email: '',
+      password: '',
+      password2: '',
+    });
+  };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -49,11 +60,45 @@ const SignUpForm = ({ setisLoggedIn }) => {
   return (
     <div className="Auth-form-container">
 
-      <div className="main-container">
+<div className="main-container">
       </div>
       <form className="Auth-form" onSubmit={handleSubmit}>
         <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign Up</h3>
+          <h3 className="Auth-form-title">
+            {authMode === 'signin' ? 'Sign In' : 'Sign Up'}
+          </h3>
+
+          <div className="text-center">
+            {authMode === 'signin' ? (
+              <span style={{fontWeight:400 , color:'grey'}}>
+                Not registered yet?{' '}
+                <span className="link-primary" onClick={changeAuthMode}>
+                  Sign Up
+                </span>
+              </span>
+            ) : (
+              <span style={{fontWeight:400 , color:'grey'}}>
+                Already registered?{' '}
+                <span className="link-primary" onClick={changeAuthMode}>
+                  Sign In
+                </span>
+              </span>
+            )}
+          </div>
+
+          {authMode === 'signup' && (
+            <div className="form-group mt-3">
+              <label>Full Name</label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                className="form-control mt-1"
+                placeholder="e.g Jane Doe"
+              />
+            </div>
+          )}
 
           <div className="form-group mt-3">
             <label>Email address</label>
@@ -65,7 +110,7 @@ const SignUpForm = ({ setisLoggedIn }) => {
               className="form-control mt-1"
               placeholder="Enter email"
             />
-            {errors.email && (<div className="text-danger">{errors.email}</div>)}
+             {errors.email && (<div className="text-danger">{errors.email}</div>)}
           </div>
 
           <div className="form-group mt-3">
@@ -81,28 +126,28 @@ const SignUpForm = ({ setisLoggedIn }) => {
             {errors.password && (<div className="text-danger">{errors.password}</div>)}
           </div>
 
-          <div className="form-group mt-3">
-            <label>Confirm Password</label>
-            <input
-              type="password"
-              name="password2"
-              value={formData.password2}
-              onChange={handleInputChange}
-              className="form-control mt-1"
-              placeholder="Confirm password"
-            />
-            {errors.password2 && (
-              <div className="text-danger">{errors.password2}</div>
-            )}
-          </div>
+          {authMode === 'signup' && (
+            <div className="form-group mt-3">
+              <label>Confirm Password</label>
+              <input
+                type="password"
+                name="password2"
+                value={formData.password2}
+                onChange={handleInputChange}
+                className="form-control mt-1"
+                placeholder="Confirm password"
+              />
+              {errors.password2 && (
+                <div className="text-danger">{errors.password2}</div>
+              )}
+            </div>
+          )}
 
           <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">Sign Up</button>
+            <button type="submit" className="btn btn-primary">
+              {authMode === 'signin' ? 'Sign In' : 'Sign Up'}
+            </button>
           </div>
-
-          {errors.general && (
-            <div className="text-danger mt-3">{errors.general}</div>
-          )}
         </div>
       </form>
       <ToastContainer
